b09a192976d020c0e793ebf19b0d01a7aad9c71f
Faris Hafizhan <farishafiz999@gmail.com>
2022-12-19 18:32:44 +0700

    feat(Zenius): add basic video presence

--- websites/Z/Zenius/presence.ts
+++ websites/Z/Zenius/presence.ts
@@ -4,15 +4,49 @@ const presence = new Presence({
 	strings = presence.getStrings({
 		play: "presence.playback.playing",
 		pause: "presence.playback.paused",
-	});
+		browsing: "general.browsing"
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+const getSection = async (presenceData: PresenceData) => {
+	const breadcrumb = document.querySelector(".breadcrumb ol");
+	const video = document.querySelector("video");
+	console.log(breadcrumb);
+	if (breadcrumb == null) {
+		presenceData.details = (await strings).browsing;
+		return presenceData;
+	}
+	if (video != null) {
+		const sectionTitle = document.querySelector('h2').textContent;
+		const videoTitle = document.querySelector('h4.c-znet-main-video-title').textContent;
+		presenceData.details = sectionTitle;
+		presenceData.state = videoTitle;
+		const timestamps = presence.getTimestampsfromMedia(video);
+		presenceData.startTimestamp = timestamps[0];
+		presenceData.endTimestamp = timestamps[1];
+		if (video.paused) {
+			delete presenceData.startTimestamp;
+			delete presenceData.endTimestamp;
+		}
+		return presenceData;
+	}
+	presenceData.details = breadcrumb.children[breadcrumb.children.length - 1].textContent;
+	return presenceData;
+}
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		details: "Zenius",
-		state: "Zenius",
-		largeImageKey: "zlogo"
+		largeImageKey: "zlogo",
+		startTimestamp: browsingTimestamp,
+		buttons: [
+			{
+				label: "Open",
+				url: document.location.href
+			},
+		]
 	};
 
-	console.log('hello zenius')
+
+	getSection(presenceData);
 	presence.setActivity(presenceData);
 });
